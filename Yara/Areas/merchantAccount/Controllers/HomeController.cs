1537d5ac54b7b5b409dd59f0d2105f1a9306e78f
nohad darweish <nohadking@hotmail.com>
2024-08-17 10:19:31 +0300

    update Merchant Arae And Faq system And Mesage by nohad in 17-8-2024 10:19 am

---
+++ Yara/Areas/merchantAccount/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Azure;
+using Domin.Entity;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Yara.Areas.merchantAccount.Controllers
+{
+    [Area("merchantAccount")]
+    [Authorize(Roles = "Admin,Merchant")]
+    public class HomeController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        IIUserInformation iUserInformation;
+        private readonly IIOrder iOrder;
+      
+        public HomeController(UserManager<ApplicationUser> userManager, IIUserInformation iUserInformation1, IIOrder iOrder)
+        {
+            _userManager = userManager;
+            iUserInformation = iUserInformation1;
+            this.iOrder = iOrder;
+         
+        }
+        public async Task<IActionResult> Index(string userId)
+        {
+            ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
+            var userd = vmodel.sUser = iUserInformation.GetById(userId);
+
+            var user = await _userManager.FindByIdAsync(userId);
+            //var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+                return NotFound();
+
+            string phoneNumber = user.PhoneNumber;
+            //if (!string.IsNullOrEmpty(phoneNumber))
+            //{
+            //    vmodel.NewOrders = (await iOrderNew.GetOrdersByPhoneAsync(phoneNumber)).ToList();
+            //    vmodel.OldOrders = (await iOrder.GetOrdersByPhoneAsync(phoneNumber)).ToList();
+            //}
+
+            return View(vmodel);
+        }
+
+       
+
+       
+    }
+}

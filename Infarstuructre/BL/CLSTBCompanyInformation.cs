d74e6ffc4d4d9fd316cff9966147606d5e881104
Alifouaad24 <161458284+Alifouaad24@users.noreply.github.com>
2024-09-04 00:45:54 -0700

    Ali finished CompanyInfoApi

--- Infarstuructre/BL/CLSTBCompanyInformation.cs
+++ Infarstuructre/BL/CLSTBCompanyInformation.cs
@@ -1,5 +1,9 @@
 ﻿
 
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using static Domin.Entity.Helper;
+
 namespace Infarstuructre.BL
 {
     public  interface IICompanyInformation
@@ -12,13 +16,24 @@ namespace Infarstuructre.BL
         List<TBCompanyInformation> GetAllv(int IdCompanyInformation);
         bool DELETPHOTO(int IdCompanyInformation);
         bool DELETPHOTOWethError(string PhotoNAme);
+        /////////////////////API///////////////////////////////
+        ///
+        Task<List<TBCompanyInformation>> GetAllAsync();
+        Task<TBCompanyInformation> GetByIdAsync(int IdMerchants);
+        Task<bool> AddDataAsync(TBCompanyInformation savee);
+        Task<bool> DeleteDataAsync(int IdMerchants);
+        Task<bool> UpdateDataAsync(TBCompanyInformation update);
+        Task<bool> DELETPHOTOASYNC(int IdMerchants);
+        Task<bool> DELETPHOTOWethErrorAsync(string PhotoNAme);
     }
     public class CLSTBCompanyInformation: IICompanyInformation
     {
         MasterDbcontext dbcontext;
-        public CLSTBCompanyInformation(MasterDbcontext dbcontext1)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CLSTBCompanyInformation(MasterDbcontext dbcontext1, IHttpContextAccessor httpContextAccessor)
         {
             dbcontext = dbcontext1;
+            _httpContextAccessor = httpContextAccessor;
         }
         public List<TBCompanyInformation> GetAll()
         {
@@ -137,6 +152,159 @@ namespace Infarstuructre.BL
                         System.IO.File.Delete(oldFilePath);
                     }
                 }
+                return true;
+            }
+            catch (Exception)
+            {
+                // يفضل ألا تترك البرنامج يتجاوز الأخطاء بصمت، يفضل تسجيل الخطأ أو إعادة رميه
+                return false;
+            }
+        }
+
+        //// ///////////////////APIs////////////////////////////////////////////////////////////////
+
+        public async Task<List<TBCompanyInformation>> GetAllAsync()
+        {
+            var scheme = _httpContextAccessor.HttpContext.Request.Scheme;
+            var host = _httpContextAccessor.HttpContext.Request.Host.Value;
+
+
+            var myDatd = await dbcontext.TBCompanyInformations.OrderByDescending(n => n.IdCompanyInformation).Where(a => a.CurrentState == true)
+                .Select( n => new TBCompanyInformation
+                {
+                    IdCompanyInformation = n.IdCompanyInformation,
+                    Photo = $"{scheme}://{host}/Images/Home/{n.Photo}",
+                    CompanyName = n.CompanyName,
+                    PhoneNumber = n.PhoneNumber,
+                    EmailCompany = n.EmailCompany,
+                    AddressEn = n.AddressEn,
+                    ShortDescriptionEn = n.ShortDescriptionEn,
+                    UrlFaceBook = n.UrlFaceBook,
+                    UrlTwitter = n.UrlTwitter,
+                    UrlInstgram = n.UrlInstgram,
+                    UrlMap = n.UrlMap,
+                    PhoneNumber2 = n.PhoneNumber2,
+                    DateTimeEntry = n.DateTimeEntry,
+                    DataEntry = n.DataEntry,
+                    CurrentState = n.CurrentState,
+
+                }).ToListAsync();
+
+            return myDatd;
+        }
+
+        public async Task<TBCompanyInformation> GetByIdAsync(int id)
+        {
+            var sslid = await dbcontext.TBCompanyInformations.FirstOrDefaultAsync(a => a.IdCompanyInformation == id);
+            return sslid;
+        }
+
+        public async Task<bool> AddDataAsync(TBCompanyInformation savee)
+        {
+            try
+            {
+                await dbcontext.AddAsync<TBCompanyInformation>(savee);
+                await dbcontext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+        public async Task<bool> DeleteDataAsync(int id)
+        {
+            try
+            {
+                var merchant = await GetByIdAsync(id);
+                merchant.CurrentState = false;
+                dbcontext.Entry(merchant).State = EntityState.Modified;
+                await dbcontext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public async Task<bool> UpdateDataAsync(TBCompanyInformation update)
+        {
+            try
+            {
+                dbcontext.Entry(update).State = EntityState.Modified;
+                await dbcontext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> DELETPHOTOASYNC(int id)
+        {
+            try
+            {
+                var catr = await GetByIdAsync(id);
+                //using (FileStream fs = new FileStream(catr.Photo, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                //{
+                if (!string.IsNullOrEmpty(catr.Photo))
+                {
+                    // إذا كان هناك صورة قديمة، قم بمسحها من الملف
+                    var oldFilePath = Path.Combine(@"wwwroot/Images/Home", catr.Photo);
+                    if (System.IO.File.Exists(oldFilePath))
+                    {
+
+
+                        // استخدم FileShare.None للسماح بحذف الملف أثناء استخدامه
+                        using (FileStream fs = new FileStream(oldFilePath, FileMode.Open, FileAccess.Read, FileShare.None))
+                        {
+                            System.Threading.Thread.Sleep(200);
+                            GC.Collect();
+                            GC.WaitForPendingFinalizers();
+                        }
+
+                        System.IO.File.Delete(oldFilePath);
+                    }
+                }
+                //}
+
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+        public async Task<bool> DELETPHOTOWethErrorAsync(string PhotoNAme)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(PhotoNAme))
+                {
+                    // إذا كان هناك صورة قديمة، قم بمسحها من الملف
+                    var oldFilePath = Path.Combine(@"wwwroot/Images/Home", PhotoNAme);
+                    if (System.IO.File.Exists(oldFilePath))
+                    {
+
+
+                        // استخدم FileShare.None للسماح بحذف الملف أثناء استخدامه
+                        using (FileStream fs = new FileStream(oldFilePath, FileMode.Open, FileAccess.Read, FileShare.None))
+                        {
+                            System.Threading.Thread.Sleep(200);
+                            GC.Collect();
+                            GC.WaitForPendingFinalizers();
+                        }
+
+                        System.IO.File.Delete(oldFilePath);
+                    }
+                }
+
                 return true;
             }
             catch (Exception)

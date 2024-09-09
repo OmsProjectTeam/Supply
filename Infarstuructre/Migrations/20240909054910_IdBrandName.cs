53260343db3b5c782c8fd1b992dcaed3de3d961a
nohad darweish <nohadking@hotmail.com>
2024-09-09 09:54:58 +0300

    update nohad

---
+++ Infarstuructre/Migrations/20240909054910_IdBrandName.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infarstuructre.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdBrandName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Make",
+                table: "TBProductInformations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "IdBrandName",
+                table: "TBProductInformations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdBrandName",
+                table: "TBProductInformations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Make",
+                table: "TBProductInformations",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- Yara/Areas/Admin/Controllers/OrderController.cs
+++ Yara/Areas/Admin/Controllers/OrderController.cs
@@ -383,7 +383,7 @@ namespace Yara.Areas.Admin.Controllers
         [HttpGet]
         public async Task<JsonResult> GetProductDetailsForOrder(string productId)
         {
-            var product = dbcontext.TBProductInformations
+            var product = dbcontext.ViewProductInformation
                          .Where(p => p.Qrcode == productId || p.Model == productId|| p.UPC == productId|| p.ProductName == productId|| p.Make == productId).FirstOrDefault();
 
 

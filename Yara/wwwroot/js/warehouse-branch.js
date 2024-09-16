476f34b44d113d3ceb55a96ac15145056b7c3ee5
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2024-09-17 02:43:05 +0600

    Refactor QR code update logic to external JS file

---
+++ Yara/wwwroot/js/warehouse-branch.js
@@ -0,0 +1,21 @@
+﻿// Function to update QR Codes for warehouse branches
+function updateQRCode() {
+    $('.QRCodeImage').each(function () {
+        var code = $(this).siblings('.Code').text();
+        if (code) {
+            $(this).attr('src', $('#QRCodeImage').data('url') + '?text=' + encodeURIComponent(code));
+        } else {
+            $(this).attr('src', '');
+        }
+    });
+}
+
+// Bind event to pagination buttons
+$(document).on('click', '.paginate_button', function () {
+    updateQRCode();
+});
+
+// Initialize on document ready
+$(document).ready(function () {
+    updateQRCode();
+});

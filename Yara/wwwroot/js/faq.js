5cc0f6b655fe61dca17626aa32e6ce3e1edb39f1
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2024-09-18 08:17:16 +0600

    Refactor JS dependencies in .cshtml files

---
+++ Yara/wwwroot/js/faq.js
@@ -0,0 +1,54 @@
+﻿$(function () {
+    $("#example2").DataTable().fnDestroy();
+    $('#example2').DataTable({
+        "paging": false,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+    });
+});
+
+$(function () {
+    $("#example3").DataTable().fnDestroy();
+    $('#example3').DataTable({
+        "paging": false,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+    });
+});
+
+$(document).ready(function () {
+    // Function to truncate text to a specified length
+    function truncateText(text, maxLength) {
+        if (text.length > maxLength) {
+            return text.substring(0, maxLength) + '...'; // Add ellipsis if text exceeds maxLength
+        } else {
+            return text;
+        }
+    }
+    // Loop through each table cell with class 'truncate-50'
+    $('.truncate-50').each(function () {
+        var text = $(this).text(); // Get the text content of the cell
+        var truncatedText = truncateText(text, 100); // Truncate the text to 50 characters
+        $(this).text(truncatedText); // Set the truncated text back to the cell
+    });
+});
+
+$(document).ready(function () {
+    console.log("Document is ready");
+    $('.select2_1').select2({
+        placeholder: "Select an option",
+        allowClear: true
+    });
+
+    // Bind change event to the selected company
+    $('#SelectFAQ').change(function () {
+        console.log("Selected FAQ changed");
+        updateCosting();
+    });
+});

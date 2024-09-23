cb5111003e77405493a925624ded82480d4660f5
masum <Administrator@WIN-56KC9DH3GB6>
2024-09-23 13:29:22 +0300

    update all

--- Yara/wwwroot/js/site.js
+++ Yara/wwwroot/js/site.js
@@ -1,25 +1,121 @@
-﻿
+﻿///// الكيوأر كود  الخاص بال ماي وير هوس برنش 
+//$(document).ready(function () {
+//    function updateQRCode() {
+//        $('.QRCodeImage').each(function () {
+//            var code = $(this).siblings('.Code').text().trim();  // قم بإزالة المسافات الفارغة من النص
+//            if (code) {
+//                var qrCodeUrl = '@Url.Action("GenerateQRCode", "WareHouse")?text=' + encodeURIComponent(code);
+//                $(this).attr('src', qrCodeUrl);
+//            } else {
+//                $(this).attr('src', '');  // في حال لم يكن هناك كود
+//            }
+//        });
+//    }
+
+//    // تحديث الـ QR Code عند النقر على أزرار التصفح
+//    $(document).on('click', '.paginate_button', function () {
+//        updateQRCode();
+//    });
+
+//    // استدعاء التحديث عند تحميل الصفحة
+//    updateQRCode();
+//});
+
 $(function () {
-    // Array of table IDs to initialize DataTables on
-    var tableIds = ["example", "example1", "example2", "example3", "example4", "example5", "example6", "example7"];
-
-    // Loop through each table ID and initialize DataTable
-    $.each(tableIds, function (index, tableId) {
-        // Destroy any existing DataTable instance on this table
-        $("#" + tableId).DataTable().fnDestroy();
-
-        // Initialize DataTable with options
-        $('#' + tableId).DataTable({
-            "paging": false,
-            "lengthChange": false,
-            "searching": false,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false
-        });
+    $("#example2").DataTable().fnDestroy();
+    $('#example2').DataTable({
+        "paging": false,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
     });
-}
-);
+});
+
+$(function () {
+    $("#example3").DataTable().fnDestroy();
+    $('#example3').DataTable({
+        "paging": false,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+    });
+});
+$(function () {
+    $("#example4").DataTable().fnDestroy();
+    $('#example4').DataTable({
+        "paging": false,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+    });
+});
+$(function () {
+    $("#example5").DataTable().fnDestroy();
+    $('#example5').DataTable({
+        "paging": false,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+    });
+});
+$(function () {
+    $("#example6").DataTable().fnDestroy();
+    $('#example6').DataTable({
+        "paging": false,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+    });
+});
+$(function () {
+    $("#example7").DataTable().fnDestroy();
+    $('#example7').DataTable({
+        "paging": false,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+    });
+});
+$(function () {
+    $("#example7").DataTable().fnDestroy();
+    $('#example7').DataTable({
+        "paging": false,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+    });
+});
+
+$(document).ready(function () {
+    // Function to truncate text to a specified length
+    function truncateText(text, maxLength) {
+        if (text.length > maxLength) {
+            return text.substring(0, maxLength) + '...'; // Add ellipsis if text exceeds maxLength
+        } else {
+            return text;
+        }
+    }
+    // Loop through each table cell with class 'truncate-50'
+    $('.truncate-50').each(function () {
+        var text = $(this).text(); // Get the text content of the cell
+        var truncatedText = truncateText(text, 50); // Truncate the text to 50 characters
+        $(this).text(truncatedText); // Set the truncated text back to the cell
+    });
+});
 
 function updateQRCode() {
     $('.QRCodeImage').each(function () {
@@ -63,5 +159,17 @@ $(document).ready(function () {
         $(this).text(truncatedText); // Set the truncated text back to the cell
     });
 });
+var loadFile = function (event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
+$("#url").val(window.location.href);
 
+
+
+
+
+
+
+  
 

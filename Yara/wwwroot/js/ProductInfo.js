7671a6b7d127d5442c78a44a38cef1e8a1be687d
Alifouaad24 <161458284+Alifouaad24@users.noreply.github.com>
2024-10-06 16:11:05 -0700

    Ali Added QRCode and BarCode To Add and My ProductInformation 6-10-2024

---
+++ Yara/wwwroot/js/ProductInfo.js
@@ -0,0 +1,41 @@
+﻿
+function updateQRCodeForPROINFOR() {
+    console.log("updateQRCodeupdateQRCodeupdateQRCodeupdateQRCodeupdateQRCode");
+    $('.QRCodeImage').each(function () {
+        var code = $(this).siblings('.Code').text();
+        if (code) {
+            
+            $(this).attr('src', 'admin/ProductInformationlowes/GenerateQRCode?text='+encodeURIComponent(code));
+        } else {
+            $(this).attr('src', '');
+        }
+    });
+}
+
+function updateBarFORPROINF() {
+    $('.BarCodeUPC').each(function () {
+        var upc = $(this).siblings('.UPCFORPR').text();
+        console.log(upc);
+        if (upc) {
+            
+            $(this).attr('src', `admin/ProductInformationlowes/GenerateBarcode?text=`+encodeURIComponent(upc));
+        } else 
+            $(this).attr('src', '');
+        }
+    });
+}
+
+
+$(document).on('click', '.paginate_button', function () {
+    updateQRCodeForPROINFOR();
+    updateBarFORPROINF();
+});
+        $(document).on('input', '.form-control .input-sm', function () {
+    updateQRCodeForPROINFOR();
+    updateBarFORPROINF();
+});
+
+$(document).ready(function () {
+    updateQRCodeForPROINFOR();
+    updateBarFORPROINF();
+});
